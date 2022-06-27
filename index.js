8dea322327c94c59d25440038dbdeddef5111d8e
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-06-27 18:11:18 +0530

    Expense Tracker

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post('/api/login', (request, response) => {
 					if (res) {
 						request.session.person_id = person_id;
 						request.session.user = results;
-						console.log(request.session.person_id);
+						console.log(request.session.user);
 						response.send(results);
 					}
 					else {
