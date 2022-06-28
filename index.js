09891f1590d6e7e71b1c234ac9c0f07630eecba2
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-06-28 06:22:31 +0530

    Expense Tracker

--- index.js
+++ index.js
@@ -211,6 +211,7 @@ app.get('/api/getmonthincome', (request, res) => {
 	if (request.session.user) {
 		const sqlSelectTotal = "select sum(Amount) as amTotal from money_additions where Type = 'Income' and person_id = ? and month(added_date) = month(now()) and year(added_date) = year(now())";
 		db.query(sqlSelectTotal, request.session.person_id, (err, result) => {
+			console.log(result);
 			res.send(result);
 		})
 	}
@@ -220,6 +221,7 @@ app.get('/api/getmonthexpense', (request, res) => {
 	if (request.session.user) {
 		const sqlSelectTotal = "select sum(Amount) as amTotal from money_additions where Type = 'Expense' and person_id = ? and month(added_date) = month(now()) and year(added_date) = year(now())";
 		db.query(sqlSelectTotal, request.session.person_id, (err, result) => {
+			console.log(result);
 			res.send(result);
 		})
 	}
